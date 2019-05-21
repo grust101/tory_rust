4d565b52d41abaf2b5257791a6a3d6ce1372c63b
grust101 <grust101@github.com>
2019-05-20 19:07:30 -0700

    update images

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,23 @@
-var elem = $.jInvertScroll(['.scroll'],        // an array containing the selector(s) for the elements you want to animate
-            {
-            height: 6000,                   // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
-            onScroll: function(percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
-                console.log(percent);
-            }
-        });
+(function($) {
+  $.jInvertScroll(['.scroll'], // an array containing the selector(s) for the elements you want to animate
+    {
+      height: 6000, // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
+      onScroll: function(percent) { //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
+      }
+    });
+
+  $('.menu a').on('click', function(e) {
+    e.preventDefault();
+    var target = '.' + $(this).data('target');
+    var left = $(target).position().left;
+    var total = $('.front.scroll').width();
+    var perc = left / (total - $(window).width()) * 100;
+
+    var height = $('html').height();
+    var final = ((height - $(window).height()) / 100 * perc);
+
+    $('html, body').animate({
+      scrollTop: final
+    }, 500);
+  });
+}(jQuery));
