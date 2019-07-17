8c20dbd88086d70813b71ba781c51a14e308dce1
Georgia Rust <georgia.rust@gmail.com>
2019-07-17 14:05:02 -0700

    create mobile image grid

--- assets/js/index.js
+++ assets/js/index.js
@@ -187,11 +187,6 @@ $(window).ready(function() {
 
 });
 
-$(window).ready(function() {
-  $(".icon").click(function() {
-    console.log("icon clicked!")
-  });
-});
 
 $(window).scroll(function() {
   if ($(window).width() > 768) {
@@ -206,12 +201,11 @@ $(window).scroll(function() {
   }
 });
 
+
 (function($) {
   $.jInvertScroll(['.scroll'], // an array containing the selector(s) for the elements you want to animate
-    {
-      height: 6000, // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
+    { // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
       onScroll: function(percent) { //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
       }
     });
-
 }(jQuery));
